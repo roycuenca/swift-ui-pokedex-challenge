e2c35c5a8de726b98be38ec54f2f664c0e548e43
Roy Cuenca <38863724+roycuenca@users.noreply.github.com>
2023-07-10 10:07:26 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Roy Cuenca on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
